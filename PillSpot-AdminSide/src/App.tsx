0a672fbb3d864f36e8615edc799a7bca63c82d02
Elaraby218 <152742814+Elaraby218@users.noreply.github.com>
2025-04-15 17:30:13 +0200

    config redux  toolkit for state managemnt

--- PillSpot-AdminSide/src/App.tsx
+++ PillSpot-AdminSide/src/App.tsx
@@ -1,17 +1,28 @@
 import "./App.css";
-import {Button} from  'antd'
+import { Flex, Splitter, Typography } from 'antd';
 
+
+
+const Desc: React.FC<Readonly<{ text?: string | number }>> = (props) => (
+  <Flex justify="center" align="center" style={{ height: '100%' }}>
+    <Typography.Title type="secondary" level={5} style={{ whiteSpace: 'nowrap' }}>
+      {props.text}
+    </Typography.Title>
+  </Flex>
+);
 
 function App() {
+ 
+
   return (
-    <>
-      <div className="text-9xl font-bold">
-        Pill spot admin side
-        <div className="App">
-          <Button type="primary">Button</Button>
-        </div>
-      </div>
-    </>
+    <Splitter style={{ height: 200, boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)' }}>
+    <Splitter.Panel defaultSize="40%" min="20%" max="70%">
+      <Desc text="First" />
+    </Splitter.Panel>
+    <Splitter.Panel>
+      <Desc text="Second" />
+    </Splitter.Panel>
+  </Splitter>
   );
 }
 
