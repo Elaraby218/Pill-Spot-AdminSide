d54b2d469dec01952d3e0aa11a9048c48303d0f6
Elaraby218 <152742814+Elaraby218@users.noreply.github.com>
2025-04-15 16:59:55 +0200

    config antd design lib

--- PillSpot-AdminSide/src/App.tsx
+++ PillSpot-AdminSide/src/App.tsx
@@ -1,16 +1,18 @@
+import "./App.css";
+import {Button} from  'antd'
 
-import './App.css'
 
 function App() {
-  
-
   return (
     <>
-      <div className='text-9xl font-bold'>
-        Pill spot admin side 
+      <div className="text-9xl font-bold">
+        Pill spot admin side
+        <div className="App">
+          <Button type="primary">Button</Button>
+        </div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
