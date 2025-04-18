d45d415026e5f3d8c25c5fda69a9e3a158df602d
Elaraby218 <152742814+Elaraby218@users.noreply.github.com>
2025-04-19 00:22:35 +0200

    create auth slice to handle login

---
+++ PillSpot-AdminSide/src/Featurs/AuthLogin/auth.ts
@@ -0,0 +1,65 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { IinitialState, IloginData, login } from "./authServices";
+import { AxiosError } from "axios";
+
+const initialState : IinitialState = {
+    userName : "" ,
+    status : 'idle' ,
+    Message : ""
+}
+
+
+
+export const Login = createAsyncThunk(
+    'auth/login',
+    async (data:IloginData , thunkAPI) => {
+        try{
+            return await login(data) ; 
+        } catch (err) {
+            if (err instanceof AxiosError) {
+              const message =
+                (err.response && err.response.data && err.response.data.message) ||
+                err.message ||
+                err.toString();
+              return thunkAPI.rejectWithValue(message);
+            }
+            return thunkAPI.rejectWithValue("An unknown error occurred");
+          }
+    }
+    
+
+)
+
+
+
+export const authLoginSlice = createSlice({
+    name : 'authLoginSlice' , 
+    initialState ,
+    reducers : {
+        resetLogin : (state) => {
+            state.status = "idle" ;
+            state.Message = "" ;
+        }
+    },
+    extraReducers : (builder) => {
+        builder
+        .addCase(Login.pending , (state)=>{
+            state.status = "loading" ;
+        })
+        .addCase(Login.fulfilled,(state,action)=>{
+            state.status = "success" ;
+            state.userName = action.payload ?? "" ;
+            state.Message = "" ;
+        })
+        .addCase(Login.rejected,(state,action)=>{
+            state.status = "error" ;
+            state.Message = action.payload as string ;
+            state.userName = "" ;
+        })
+    }
+});
+
+
+
+export const {resetLogin} = authLoginSlice.actions ;
+export default authLoginSlice.reducer ;
---
+++ PillSpot-AdminSide/src/Featurs/AuthLogin/authServices.ts
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+export interface IcurAdmin {
+    userName : string ;
+    firstName : string ;
+    lastName : string ;
+    userId : string ;
+    email:string ;
+}
+
+export interface IinitialState {
+    userName : string ,
+    status : "idle" | "loading" | "error" | "success" ;
+    Message : string ,
+}
+
+export interface IloginData {
+    userName : string ;
+    password : string ;
+}
+
+const apiUrl = import.meta.env.VITE_LOGIN_URL;
+
+export const login = async (userData: IloginData) => {
+    const response = await axios.post(apiUrl, userData, {withCredentials:true} );
+    if (response.data) return userData.userName;
+};
